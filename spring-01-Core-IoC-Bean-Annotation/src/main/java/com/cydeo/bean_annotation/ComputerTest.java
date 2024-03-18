9b2e00677fdf01d17230edb273abf68c9ccf62c6
gozdediri <dirigozde@gmail.com>
2024-03-17 23:31:38 -0500

    Core-01

---
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -0,0 +1,15 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ComputerTest {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new AnnotationConfigApplicationContext();
+
+
+
+    }
+}
