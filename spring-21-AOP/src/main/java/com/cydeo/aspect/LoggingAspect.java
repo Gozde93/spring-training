312c70f95443f059ae38243608870078e8ee93c4
gozdediri <dirigozde@gmail.com>
2024-07-11 13:09:19 -0500

    AOP

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -17,6 +17,101 @@ public class LoggingAspect {
   Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
   //point that is for defining my joint point
-  @Pointcut()
+//  @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))")
+//  public void myPointcut(){}
+//
+//  @Before(" myPointcut()")
+//  public void log(){
+//    logger.info("Info log.........");
+//  }
+
+//  @Before("execution(* com.cydeo.controller.CourseController.*(..))")
+//  public void log(){
+//    logger.info("Info log.........");
+//  }
+
+// @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))")
+////    public void courseRepositoryFindByIdPC() {}
+////
+////    @Before("courseRepositoryFindByIdPC()")
+////    public void beforeCourseRepositoryFindById(JoinPoint joinPoint) {
+////        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+////                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+////    }
+
+//  @Pointcut("within(com.cydeo.controller..*)")
+//  public void anyControllerOperation(){}
+//
+//  @Pointcut("@within(org.springframework.stereotype.Service)")
+//  public void anyServiceOperation(){}
+//
+//  @Before("anyControllerOperation() || anyServiceOperation()")
+//  public void beforeControllerOrServiceAdvice(JoinPoint joinPoint) {
+//
+//    logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//            , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//  }
+
+//  @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+//  public void anyDeleteControllerOperation(){}
+//
+//  @Before("anyDeleteControllerOperation()")
+//  public void beforeDeleteMappingAnnotation(JoinPoint joinPoint){
+//    logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
+//           , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//  }
+
+//  @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
+//  public void loggingAnnotationPC(){}
+//
+//  @Before("loggingAnnotationPC()")
+//   public void beforeDeleteMappingAnnotation(JoinPoint joinPoint){
+//    logger.info("Before -> Method: {}, Arguments: {}, Target: {}", joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
+//  }
+
+  @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+  public void afterReturningGetMappingAnnotation(){}
+
+//  @AfterReturning(pointcut = "afterReturningGetMappingAnnotation()",returning = "result")
+//  public void afterGetMappingAnnotation(JoinPoint joinPoint,Object result){
+//    logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(),result.toString());
+//  }
+
+//  @AfterReturning(pointcut = "afterReturningGetMappingAnnotation()",returning = "results")
+//  public void afterGetMappingAnnotation(JoinPoint joinPoint,List<CourseDTO> results){
+//    logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(),results.toString());
+//  }
+
+//  @AfterThrowing(pointcut = "afterReturningGetMappingAnnotation()", throwing = "exception")
+//  public void afterThrowingGetMappingOperation(JoinPoint joinPoint,RuntimeException exception){
+//    logger.error("After Throwing -> Method: {}, Exception:{}"
+//    ,joinPoint.getSignature().toString(), exception.getMessage());
+//  }
+
+  @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
+    public void loggingAnnotationPC(){}
+
+  @Around("loggingAnnotationPC()")
+  public Object annyLoggingAnnotationOperation(ProceedingJoinPoint proceedingJoinPoint){
+    logger.info("Before -> Method: {} - Parameter{}", proceedingJoinPoint.getSignature().toShortString(),proceedingJoinPoint.getArgs());
+
+    Object result = null;
+
+    try {
+      result = proceedingJoinPoint.proceed();
+    }catch (Throwable throwable){
+      throwable.printStackTrace();
+    }
+
+    logger.info("After -> Method: {} - Result: {}", proceedingJoinPoint.getSignature().toShortString(),result.toString());
+
+    return result;
+  }
+
+
+
+
+
+
 
 }
